0350a2efe981486008d2dc65fa92404d6bd76b36
Tim Holzer <timiholzer@gmail.com>
2021-01-23 19:45:03 -0700

    updated mongoose connection

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ require("./routes/apiroutes.js")(app);
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify:false
 })
 
