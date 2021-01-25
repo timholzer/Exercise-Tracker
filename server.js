0575768ada0a1fee1aa1615304e4861dd8a988fc
Tim Holzer <timiholzer@gmail.com>
2021-01-24 21:07:27 -0700

    moved paths

--- server.js
+++ server.js
@@ -27,3 +27,20 @@ mongoose.connect(
 app.listen(PORT,function(){ 
     console.log(`App listening on Port ${PORT}`);
 });
+
+var path = require("path");
+
+module.exports = function(app){ 
+  app.get("/exercise",function (req,res){   
+      res.sendFile(path.join(__dirname,"../public/exercise.html"));
+  });
+
+  app.get("/",function(req,res){    
+      res.sendFile(path.join(__dirname,"../public/index.html"));
+  });
+
+  app.get("/stats",function(req,res){   
+      res.sendFile(path.join(__dirname,"../public/stats.html"));
+  });
+
+}
